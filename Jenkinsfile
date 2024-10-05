ba553853cd34f1243a8b89059689ae7469e9d2bb
RayenBal <medrayen.balghouthi@esprit.tn>
2024-10-05 21:03:00 +0100

    Commit 3

---
+++ Jenkinsfile
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+
+    tools {
+        jdk 'JAVA_HOME'
+        maven 'M2_HOME'
+    }
+
+    stages {
+        stage('GIT') {
+            steps {
+                git branch: 'Nawelhammami-5NIDS1-G1', 
+                url: 'https://github.com/MohamedKhalil-Mzali/5NIDS-G1-ProjetDevOps.git'
+            }
+        }
+
+        stage('Compile Stage') {
+            steps {
+                sh 'mvn clean compile'
+            }
+        }
+    }
+}
--- gestion-station-ski/src/main/java/tn/esprit/spring/services/PisteServicesImpl.java
+++ gestion-station-ski/src/main/java/tn/esprit/spring/services/PisteServicesImpl.java
@@ -4,31 +4,36 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import tn.esprit.spring.entities.Piste;
 import tn.esprit.spring.repositories.IPisteRepository;
-
+import lombok.extern.sl4j.Sl4j
 import java.util.List;
 @AllArgsConstructor
 @Service
+@Sl4j
 public class PisteServicesImpl implements  IPisteServices{
 
     private IPisteRepository pisteRepository;
 
     @Override
     public List<Piste> retrieveAllPistes() {
+      log.info("afficher tout");
         return pisteRepository.findAll();
     }
 
     @Override
     public Piste addPiste(Piste piste) {
+       log.info("ajouter");
         return pisteRepository.save(piste);
     }
 
     @Override
     public void removePiste(Long numPiste) {
+     log.info("supprimer");
         pisteRepository.deleteById(numPiste);
     }
 
     @Override
     public Piste retrievePiste(Long numPiste) {
+            log.info("afficher ");
         return pisteRepository.findById(numPiste).orElse(null);
     }
 }
