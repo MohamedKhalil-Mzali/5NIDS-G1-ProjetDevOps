32fbe5a1a566a979612ae7423bb5a7d04814e1ec
nawelhammami12 <124445122+nawelhammami12@users.noreply.github.com>
2024-10-20 20:10:15 +0200

    Update SubscriptionServicesImpl.java

--- src/main/java/tn/esprit/spring/services/SubscriptionServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/SubscriptionServicesImpl.java
@@ -7,52 +7,46 @@ import org.springframework.stereotype.Service;
 import tn.esprit.spring.entities.Skier;
 import tn.esprit.spring.entities.Subscription;
 import tn.esprit.spring.entities.TypeSubscription;
-import tn.esprit.spring.exceptions.EntityNotFoundException;
 import tn.esprit.spring.repositories.ISkierRepository;
 import tn.esprit.spring.repositories.ISubscriptionRepository;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
 @AllArgsConstructor
 @Service
-public class SubscriptionServicesImpl implements ISubscriptionServices {
+public class SubscriptionServicesImpl implements ISubscriptionServices{
 
-    private final ISubscriptionRepository subscriptionRepository;
-    private final ISkierRepository skierRepository;
+    private ISubscriptionRepository subscriptionRepository;
+
+    private ISkierRepository skierRepository;
 
     @Override
     public Subscription addSubscription(Subscription subscription) {
-        subscription.setEndDate(calculateEndDate(subscription));
-        return subscriptionRepository.save(subscription);
-    }
-
-    private LocalDate calculateEndDate(Subscription subscription) {
         switch (subscription.getTypeSub()) {
             case ANNUAL:
-                return subscription.getStartDate().plusYears(1);
+                subscription.setEndDate(subscription.getStartDate().plusYears(1));
+                break;
             case SEMESTRIEL:
-                return subscription.getStartDate().plusMonths(6);
+                subscription.setEndDate(subscription.getStartDate().plusMonths(6));
+                break;
             case MONTHLY:
-                return subscription.getStartDate().plusMonths(1);
-            default:
-                throw new IllegalArgumentException("Invalid subscription type");
+                subscription.setEndDate(subscription.getStartDate().plusMonths(1));
+                break;
         }
+        return subscriptionRepository.save(subscription);
     }
 
     @Override
     public Subscription updateSubscription(Subscription subscription) {
-        subscription.setEndDate(calculateEndDate(subscription)); // Ensure end date is updated
         return subscriptionRepository.save(subscription);
     }
 
     @Override
     public Subscription retrieveSubscriptionById(Long numSubscription) {
-        return subscriptionRepository.findById(numSubscription)
-                .orElseThrow(() -> new EntityNotFoundException("Subscription not found"));
+        return subscriptionRepository.findById(numSubscription).orElse(null);
     }
 
     @Override
@@ -66,25 +60,30 @@ public class SubscriptionServicesImpl implements ISubscriptionServices {
     }
 
     @Override
-    @Scheduled(cron = "*/30 * * * * *")
-    public void logSubscriptions() {
-        subscriptionRepository.findDistinctOrderByEndDateAsc().forEach(sub -> {
-            Optional<Skier> aSkierOpt = skierRepository.findBySubscription(sub);
-            aSkierOpt.ifPresent(aSkier -> 
-                log.info("{} | {} | {} {}", sub.getNumSub(), sub.getEndDate(), aSkier.getFirstName(), aSkier.getLastName())
-            );
-        });
+    @Scheduled(cron = "*/30 * * * * *") /* Cron expression to run a job every 30 secondes */
+    public void retrieveSubscriptions() {
+        for (Subscription sub: subscriptionRepository.findDistinctOrderByEndDateAsc()) {
+            Skier   aSkier = skierRepository.findBySubscription(sub);
+            log.info(sub.getNumSub().toString() + " | "+ sub.getEndDate().toString()
+                    + " | "+ aSkier.getFirstName() + " " + aSkier.getLastName());
+        }
     }
 
-    @Scheduled(cron = "0 0 * * * *") // Runs hourly for demonstration
+    @Scheduled(cron = "*/30 * * * * *") // Cron expression to run a job every 30 seconds
     public void showMonthlyRecurringRevenue() {
-        Float monthlyRevenue = Optional.ofNullable(subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.MONTHLY)).orElse(0f);
-        Float semestrielRevenue = Optional.ofNullable(subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.SEMESTRIEL)).orElse(0f) / 6;
-        Float annualRevenue = Optional.ofNullable(subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.ANNUAL)).orElse(0f) / 12;
+        Float monthlyRevenue = subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.MONTHLY);
+        Float semestrielRevenue = subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.SEMESTRIEL);
+        Float annualRevenue = subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.ANNUAL);
 
-        Float totalRevenue = monthlyRevenue + semestrielRevenue + annualRevenue;
-        log.info("Monthly Revenue = {}", totalRevenue);
+        // Use default values of 0 if any revenue is null
+        monthlyRevenue = (monthlyRevenue != null) ? monthlyRevenue : 0;
+        semestrielRevenue = (semestrielRevenue != null) ? semestrielRevenue / 6 : 0;
+        annualRevenue = (annualRevenue != null) ? annualRevenue / 12 : 0;
+
+        Float revenue = monthlyRevenue + semestrielRevenue + annualRevenue;
+        log.info("Monthly Revenue = " + revenue);
     }
-}
 
+
+}
 
