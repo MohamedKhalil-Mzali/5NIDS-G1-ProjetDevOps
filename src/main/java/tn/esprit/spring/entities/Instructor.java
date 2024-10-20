ddf67af9f94e4cc32765254656b33388e780a0e0
nawelhammami12 <124445122+nawelhammami12@users.noreply.github.com>
2024-10-20 19:08:15 +0200

    Update Instructor.java

--- src/main/java/tn/esprit/spring/entities/Instructor.java
+++ src/main/java/tn/esprit/spring/entities/Instructor.java
@@ -3,13 +3,16 @@ package tn.esprit.spring.entities;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Set;
+
 import jakarta.persistence.*;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+
 
 @Getter
 @Setter
@@ -18,15 +21,12 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level=AccessLevel.PRIVATE)
 @Entity
 public class Instructor implements Serializable {
-
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    Long numInstructor;
-    String firstName;
-    String lastName;
-    LocalDate dateOfHire;
-
-    @OneToMany(mappedBy = "instructor") // Si Course a une relation vers Instructor
-    transient Set<Course> courses; // Ou assure-toi que Course est sérialisable
-}
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	Long numInstructor;
+	String firstName;
+	String lastName;
+	LocalDate dateOfHire;
+	@OneToMany
+	Set<Course> courses;
+} 
