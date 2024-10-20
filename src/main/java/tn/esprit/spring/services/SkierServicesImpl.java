ce32cd6ec1913862739bd5e88577e04a5ffd0586
nawelhammami12 <124445122+nawelhammami12@users.noreply.github.com>
2024-10-20 18:44:23 +0200

    Update SkierServicesImpl.java

--- src/main/java/tn/esprit/spring/services/SkierServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/SkierServicesImpl.java
@@ -7,22 +7,18 @@ import tn.esprit.spring.repositories.*;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @AllArgsConstructor
 @Service
 public class SkierServicesImpl implements ISkierServices {
 
-    private ISkierRepository skierRepository;
-
-    private IPisteRepository pisteRepository;
-
-    private ICourseRepository courseRepository;
-
-    private IRegistrationRepository registrationRepository;
-
-    private ISubscriptionRepository subscriptionRepository;
-
+    private final ISkierRepository skierRepository;
+    private final IPisteRepository pisteRepository;
+    private final ICourseRepository courseRepository;
+    private final IRegistrationRepository registrationRepository;
+    private final ISubscriptionRepository subscriptionRepository;
 
     @Override
     public List<Skier> retrieveAllSkiers() {
@@ -31,37 +27,47 @@ public class SkierServicesImpl implements ISkierServices {
 
     @Override
     public Skier addSkier(Skier skier) {
-        switch (skier.getSubscription().getTypeSub()) {
-            case ANNUAL:
-                skier.getSubscription().setEndDate(skier.getSubscription().getStartDate().plusYears(1));
-                break;
-            case SEMESTRIEL:
-                skier.getSubscription().setEndDate(skier.getSubscription().getStartDate().plusMonths(6));
-                break;
-            case MONTHLY:
-                skier.getSubscription().setEndDate(skier.getSubscription().getStartDate().plusMonths(1));
-                break;
+        if (skier.getSubscription() != null) {
+            switch (skier.getSubscription().getTypeSub()) {
+                case ANNUAL:
+                    skier.getSubscription().setEndDate(skier.getSubscription().getStartDate().plusYears(1));
+                    break;
+                case SEMESTRIEL:
+                    skier.getSubscription().setEndDate(skier.getSubscription().getStartDate().plusMonths(6));
+                    break;
+                case MONTHLY:
+                    skier.getSubscription().setEndDate(skier.getSubscription().getStartDate().plusMonths(1));
+                    break;
+            }
         }
         return skierRepository.save(skier);
     }
 
     @Override
     public Skier assignSkierToSubscription(Long numSkier, Long numSubscription) {
-        Skier skier = skierRepository.findById(numSkier).orElse(null);
-        Subscription subscription = subscriptionRepository.findById(numSubscription).orElse(null);
-        skier.setSubscription(subscription);
-        return skierRepository.save(skier);
+        Optional<Skier> skierOpt = skierRepository.findById(numSkier);
+        Optional<Subscription> subscriptionOpt = subscriptionRepository.findById(numSubscription);
+
+        if (skierOpt.isPresent() && subscriptionOpt.isPresent()) {
+            Skier skier = skierOpt.get();
+            skier.setSubscription(subscriptionOpt.get());
+            return skierRepository.save(skier);
+        }
+        return null; // Handle case where skier or subscription doesn't exist
     }
 
     @Override
     public Skier addSkierAndAssignToCourse(Skier skier, Long numCourse) {
         Skier savedSkier = skierRepository.save(skier);
-        Course course = courseRepository.getById(numCourse);
-        Set<Registration> registrations = savedSkier.getRegistrations();
-        for (Registration r : registrations) {
-            r.setSkier(savedSkier);
-            r.setCourse(course);
-            registrationRepository.save(r);
+        Course course = courseRepository.findById(numCourse).orElse(null);
+
+        if (course != null) {
+            Set<Registration> registrations = savedSkier.getRegistrations();
+            for (Registration r : registrations) {
+                r.setSkier(savedSkier);
+                r.setCourse(course);
+                registrationRepository.save(r);
+            }
         }
         return savedSkier;
     }
@@ -78,17 +84,22 @@ public class SkierServicesImpl implements ISkierServices {
 
     @Override
     public Skier assignSkierToPiste(Long numSkieur, Long numPiste) {
-        Skier skier = skierRepository.findById(numSkieur).orElse(null);
-        Piste piste = pisteRepository.findById(numPiste).orElse(null);
-        try {
-            skier.getPistes().add(piste);
-        } catch (NullPointerException exception) {
-            Set<Piste> pisteList = new HashSet<>();
-            pisteList.add(piste);
-            skier.setPistes(pisteList);
-        }
+        Optional<Skier> skierOpt = skierRepository.findById(numSkieur);
+        Optional<Piste> pisteOpt = pisteRepository.findById(numPiste);
 
-        return skierRepository.save(skier);
+        if (skierOpt.isPresent() && pisteOpt.isPresent()) {
+            Skier skier = skierOpt.get();
+            Piste piste = pisteOpt.get();
+            Set<Piste> pistes = skier.getPistes();
+
+            if (pistes == null) {
+                pistes = new HashSet<>();
+                skier.setPistes(pistes);
+            }
+            pistes.add(piste);
+            return skierRepository.save(skier);
+        }
+        return null; // Handle case where skier or piste doesn't exist
     }
 
     @Override
@@ -96,3 +107,4 @@ public class SkierServicesImpl implements ISkierServices {
         return skierRepository.findBySubscription_TypeSub(typeSubscription);
     }
 }
+
