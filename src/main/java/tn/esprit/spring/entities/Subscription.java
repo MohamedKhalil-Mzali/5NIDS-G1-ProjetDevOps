2d8fbdc406a150ffc017ba30e314f67824ac1f6f
nawelhammami12 <124445122+nawelhammami12@users.noreply.github.com>
2024-10-20 20:06:11 +0200

    Update Subscription.java

--- src/main/java/tn/esprit/spring/entities/Subscription.java
+++ src/main/java/tn/esprit/spring/entities/Subscription.java
@@ -1,34 +1,22 @@
 package tn.esprit.spring.entities;
 
-import java.io.Serializable;
 import java.time.LocalDate;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import lombok.experimental.FieldDefaults;
+import lombok.*;
 
 @Getter
 @Setter
-@ToString
-@AllArgsConstructor
 @NoArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
 @Entity
-public class Subscription implements Serializable {
+public class Subscription {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long numSub;
-
-    LocalDate startDate;
-    LocalDate endDate;
-    Float price;
+    private Long numSubscription;
+    private LocalDate startDate;
+    private LocalDate endDate;
 
     @Enumerated(EnumType.STRING)
-    TypeSubscription typeSub;
+    private TypeSubscription typeSub; // Ensure TypeSubscription is defined correctly
 }
-
