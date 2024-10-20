4179dced58a2e591a0ef855b971c95976f947ab8
nawelhammami12 <124445122+nawelhammami12@users.noreply.github.com>
2024-10-20 20:05:48 +0200

    Update Skier.java

--- src/main/java/tn/esprit/spring/entities/Skier.java
+++ src/main/java/tn/esprit/spring/entities/Skier.java
@@ -5,47 +5,35 @@ import java.time.LocalDate;
 import java.util.Set;
 
 import jakarta.persistence.*;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.FieldDefaults;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@FieldDefaults(level=AccessLevel.PRIVATE)
 @Entity
 public class Skier implements Serializable {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	Long numSkier;
-	String firstName;
-	String lastName;
-	LocalDate dateOfBirth;
-	String city;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long numSkier;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String city;
 
-	@OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
-	Subscription subscription;
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private Subscription subscription;
 
-	@JsonIgnore
-	@ManyToMany
-	@JoinTable(
-			name = "excursion",
-			joinColumns = @JoinColumn(name = "numSkier"),
-			inverseJoinColumns = @JoinColumn(name = "numPiste"))
-	private Set<Piste> pistes;
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+        name = "excursion",
+        joinColumns = @JoinColumn(name = "numSkier"),
+        inverseJoinColumns = @JoinColumn(name = "numPiste"))
+    private Set<Piste> pistes;
 
-
-	@OneToMany(mappedBy = "skier")
-	Set<Registration> registrations;
-
-
-
-} 
-
+    @OneToMany(mappedBy = "skier")
+    private Set<Registration> registrations;
+}
