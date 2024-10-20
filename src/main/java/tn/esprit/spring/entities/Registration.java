aec5237c3d02156746d9db4dc7b93f2f9081a362
nawelhammami12 <124445122+nawelhammami12@users.noreply.github.com>
2024-10-20 20:08:03 +0200

    Update Registration.java

--- src/main/java/tn/esprit/spring/entities/Registration.java
+++ src/main/java/tn/esprit/spring/entities/Registration.java
@@ -1,38 +1,36 @@
 package tn.esprit.spring.entities;
 
 import java.io.Serializable;
-import java.util.Set;
+
 import jakarta.persistence.*;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.FieldDefaults;
-
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level=AccessLevel.PRIVATE)
 @Entity
-public class Course implements Serializable {
+public class Registration implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	Long numCourse;
-	int level;
-
-	@Enumerated(EnumType.STRING)
-	TypeCourse typeCourse;
-
-	@Enumerated(EnumType.STRING)
-	Support support;
-	Float price;
-	int timeSlot;
+	Long numRegistration;
+	int numWeek;
 
 	@JsonIgnore
-	@OneToMany(mappedBy = "course")
-	transient Set<Registration> registrations; // ou assure-toi que Registration est sérialisable
+	@ManyToOne
+    Skier skier;
+	@JsonIgnore
+	@ManyToOne
+	Course course;
 }
+
