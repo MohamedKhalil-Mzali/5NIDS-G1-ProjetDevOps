070032dcb67769401c6f59b1e2d4e59df66ecd7f
nawelhammami12 <124445122+nawelhammami12@users.noreply.github.com>
2024-10-23 11:34:07 +0200

    Update ISkierServices.java

--- src/main/java/tn/esprit/spring/services/ISkierServices.java
+++ src/main/java/tn/esprit/spring/services/ISkierServices.java
@@ -6,22 +6,12 @@ import tn.esprit.spring.entities.TypeSubscription;
 import java.util.List;
 
 public interface ISkierServices {
-
 	List<Skier> retrieveAllSkiers();
-
-	Skier  addSkier(Skier  skier);
-
+	Skier addSkier(Skier skier);
 	Skier assignSkierToSubscription(Long numSkier, Long numSubscription);
-
 	Skier addSkierAndAssignToCourse(Skier skier, Long numCourse);
-
-	void removeSkier (Long numSkier);
-
-	Skier retrieveSkier (Long numSkier);
-
-
+	void removeSkier(Long numSkier);
+	Skier retrieveSkier(Long numSkier);
 	Skier assignSkierToPiste(Long numSkieur, Long numPiste);
-
 	List<Skier> retrieveSkiersBySubscriptionType(TypeSubscription typeSubscription);
-
 }
