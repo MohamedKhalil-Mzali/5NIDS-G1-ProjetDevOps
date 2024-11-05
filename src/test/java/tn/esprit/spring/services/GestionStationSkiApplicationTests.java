9a1c01dc05bdcc15fc6280648c61ab89f0a227cb
nawelhammami12 <124445122+nawelhammami12@users.noreply.github.com>
2024-11-05 16:50:21 +0100

    Update GestionStationSkiApplicationTests.java

--- src/test/java/tn/esprit/spring/services/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/services/GestionStationSkiApplicationTests.java
@@ -13,6 +13,7 @@ import tn.esprit.spring.entities.Support; // Assurez-vous d'importer l'énuméra
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
